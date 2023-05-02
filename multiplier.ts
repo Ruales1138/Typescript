2cd3dd5b21648d83fc9f3883468241a3a0839bcf
unknown <hruales1138@gmail.com>
2023-05-02 18:47:02 -0500

    Commit

--- multiplier.ts
+++ multiplier.ts
@@ -2,4 +2,25 @@ const multiplicator = (a: number, b: number, printText: string) => {
     console.log(printText, a * b)
 }
 
-multiplicator(2, 4, 'Multipliet number 2 and 4, the result is: ')
+const parseArguments = (args): number[] => {
+    if(args.length !== 4) throw new Error('Wrong number of arguments')
+
+    const firstNumber = Number(args[2])
+    const secondNumber = Number(args[3])
+
+    if(!isNaN(firstNumber) && !isNaN(secondNumber)) {
+        return [
+            firstNumber,
+            secondNumber
+        ]
+    }
+        
+    throw new Error('Provided values were not numbers!')
+}
+
+const cleanArguments = parseArguments(process.argv)
+
+const a: number = Number(cleanArguments[0])
+const b: number = Number(cleanArguments[1])
+
+multiplicator(a, b, `Multipliet ${a} and ${b}, the result is: `)
